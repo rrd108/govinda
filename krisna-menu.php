fa59a02253055f5b9808fbadd21f0224189853ff
rrd <rrd@1108.cc>
2021-01-20 15:33:27 +0100

    fix menu

--- krisna-menu.php
+++ krisna-menu.php
@@ -5,15 +5,15 @@
         <span></span>
         <span></span>
         <ul id="menu">
-            <li><a href="http://lelekpalotaja.hu">Budapest</a></li>
-            <li><a href="http://karitativ.hu">Ételosztás</a></li>
-            <li><a href="http://govinda.hu">Éttermek</a></li>
-            <li><a href="http://bhf.hu">Főiskola</a></li>
-            <li><a href="http://krisnavolgy.hu">Krisna-völgy</a></li>
-            <li><a href="http://krisna.hu">krisna.hu</a></li>
-            <li><a href="http://ujkrisnahu.1108.cc/1-2/">1%</a></li>
-            <li><a href="http://okovolgy.hu">Öko-völgy</a></li>
+            <li><a href="https://krisna.hu">krisna.hu</a></li>
+            <li><a href="https://http://veganporta.hu/">VegánPorta</a></li>
             <li><a href="https://bolt.krisnavolgy.hu">Webshop</a></li>
+            <li><a href="https://lelekpalotaja.hu">Lélek Palotája</a></li>
+            <li><a href="https://karitativ.hu">Ételosztás</a></li>
+            <li><a href="https://bhf.hu">Főiskola</a></li>
+            <li><a href="https://krisnavolgy.hu">Krisna-völgy</a></li>
+            <li><a href="https://egyhazi.hu">1%</a></li>
+            <li><a href="https://okovolgy.hu">Öko-völgy</a></li>
         </ul>
     </div>
 </nav>
