2fd0d941d22513c88aee4b93b319860bd2eccd91
rrd <rrd@1108.cc>
2021-01-21 16:06:54 +0100

    fix footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+  <div>
+    <h1>Nézd meg ezt is</h1>
+    <ul>
+      <li><a href="https://bolt.krisnavolgy.hu/?s=szak%C3%A1csk%C3%B6nyv&post_type=product&type_aws=true">Szakácskönyvek</a></li>
+      <li><a href="https://vegavarazs.hu">vegavarazs.hu</a></li>
+    </ul>
+  </div>
+</footer>
--- index.php
+++ index.php
@@ -184,68 +184,8 @@
     </article>
     <?php endif; ?>
   </main>
-  <footer>
-      <div>
-      <h3>Krisna.hu hírlevél feliratkozás</h3>
-      <!-- Begin MailChimp Signup Form -->
-          <form action="//krisna.us7.list-manage.com/subscribe/post?u=02a57b758ba9a1c47589e529b&amp;id=a6fbd1031f"
-                method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
-                target="_blank" novalidate="novalidate">
-              <label for="mce-EMAIL">E-mail <span class="asterisk">*</span></label>
-              <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" aria-required="true">
-              <label for="mce-FNAME">Név <span class="asterisk">*</span></label>
-              <input type="text" value="" name="FNAME" class="required" id="mce-FNAME" aria-required="true">
-              <strong><a href="http://krisna.hu/kapcsolat/internetes-adatvedelmi-iranyelvek/"
-                          style="text-decoration:none;" target="_blank" ;="">Adatvédelmi nyilatkozat »</a> <span
-                          class="asterisk">*</span></strong>
-              <input type="radio" value="Elfogadom" name="MMERGE2" id="mce-MMERGE2-0">
-              <label for="mce-MMERGE2-0">Elfogadom</label>
-              <div id="mce-responses" class="clear">
-                  <div class="response" id="mce-error-response" style="display:none"></div>
-                  <div class="response" id="mce-success-response" style="display:none"></div>
-              </div>
-              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-              <div style="position: absolute; left: -5000px;">
-                  <input type="text" name="b_02a57b758ba9a1c47589e529b_a6fbd1031f" tabindex="-1" value="">
-              </div>
-              <div class="indicates-required"><span class="asterisk">*</span> a mezők kitöltése szükséges</div>
-              <div class="clear">
-                  <input type="submit" value="Feliratkozás" name="subscribe" id="mc-embedded-subscribe"
-                          class="button2">
-              </div>
-      </form>
 
-      <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
-      <script type="text/javascript">(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='MMERGE2';ftypes[2]='radio'; /*
-      * Translated default messages for the $ validation plugin.
-      * Locale: HU
-      */
-              $.extend($.validator.messages, {
-                  required: "Kötelező megadni.",
-                  maxlength: $.validator.format("Legfeljebb {0} karakter hosszú legyen."),
-                  minlength: $.validator.format("Legalább {0} karakter hosszú legyen."),
-                  rangelength: $.validator.format("Legalább {0} és legfeljebb {1} karakter hosszú legyen."),
-                  email: "Érvényes e-mail címnek kell lennie.",
-                  url: "Érvényes URL-nek kell lennie.",
-                  date: "Dátumnak kell lennie.",
-                  number: "Számnak kell lennie.",
-                  digits: "Csak számjegyek lehetnek.",
-                  equalTo: "Meg kell egyeznie a két értéknek.",
-                  range: $.validator.format("{0} és {1} közé kell esnie."),
-                  max: $.validator.format("Nem lehet nagyobb, mint {0}."),
-                  min: $.validator.format("Nem lehet kisebb, mint {0}."),
-                  creditcard: "Érvényes hitelkártyaszámnak kell lennie."
-              });}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-      <!--End mc_embed_signup-->
-      </div>
-      <div>
-          <h3>Linkek</h3>
-          <ul>
-              <li><a href="https://bolt.krisnavolgy.hu/kereses?tag=szak%C3%A1csk%C3%B6nyv">Szakácskönyvek</a></li>
-          </ul>
-      </div>
+  <?php require "./footer.php"; ?>
 
-  </footer>
 </body>
-
 </html>
