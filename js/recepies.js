82133d244f468fa450328ee0026d88d2131a18e6
rrd <rrd@1108.cc>
2021-04-26 14:03:45 +0200

    fix error on missing picture

--- js/recepies.js
+++ js/recepies.js
@@ -1,27 +1,46 @@
 const recepies = document.getElementById('recepies')
 
-const getRecepie = (recepie) => {
-  const hasHyphen = recepie.title.rendered.substr(0, recepie.title.rendered.indexOf('&#8211;'))
-  let name = hasHyphen ? recepie.title.rendered.substr(0, recepie.title.rendered.indexOf('&#8211;')) : recepie.title.rendered
+const getRecepie = recepie => {
+  const hasHyphen = recepie.title.rendered.substr(
+    0,
+    recepie.title.rendered.indexOf('&#8211;')
+  )
+  let name = hasHyphen
+    ? recepie.title.rendered.substr(
+        0,
+        recepie.title.rendered.indexOf('&#8211;')
+      )
+    : recepie.title.rendered
   // 8 chars is space + &#8211; + space
-  let description = hasHyphen ? recepie.title.rendered.substr(recepie.title.rendered.indexOf('&#8211;') + 8) : '&nbsp;'
+  let description = hasHyphen
+    ? recepie.title.rendered.substr(
+        recepie.title.rendered.indexOf('&#8211;') + 8
+      )
+    : '&nbsp;'
 
-  let picture = recepie.content.rendered.match(/<img\b(?=\s)(?=(?:[^>=]|='[^']*'|="[^"]*"|=[^'"][^\s>]*)*?\ssrc=['"]([^"]*)['"]?)(?:[^>=]|='[^']*'|="[^"]*"|=[^'"\s]*)*"\s?\/?>/)
+  let picture = recepie.content.rendered.match(
+    /<img\b(?=\s)(?=(?:[^>=]|='[^']*'|="[^"]*"|=[^'"][^\s>]*)*?\ssrc=['"]([^"]*)['"]?)(?:[^>=]|='[^']*'|="[^"]*"|=[^'"\s]*)*"\s?\/?>/
+  )
 
-  recepies.innerHTML += `<div>
+  if (picture) {
+    recepies.innerHTML += `<div>
     <img loading="lazy" src="${picture[1]}" alt="${name}">
     <h3>${name}</h3>
     <p>${description}</p>
     <a href="${recepie.link}" class="button">Recept »</a>
   </div>`
+  }
 }
 
-fetch('https://krisna.hu/wp-json/wp/v2/posts?categories=31&per_page=3&_fields=id,link,title.rendered,content.rendered')
+fetch(
+  'https://krisna.hu/wp-json/wp/v2/posts?categories=31&per_page=3&_fields=id,link,title.rendered,content.rendered'
+)
   .then(response => response.json())
   .then(data => {
     data.forEach(recepie => getRecepie(recepie))
-
-    fetch('https://vegavarazs.hu/wp-json/wp/v2/posts?categories=1&per_page=3&_fields=id,link,title.rendered,content.rendered')
+    fetch(
+      'https://vegavarazs.hu/wp-json/wp/v2/posts?categories=1&per_page=3&_fields=id,link,title.rendered,content.rendered'
+    )
       .then(response => response.json())
       .then(data => data.forEach(recepie => getRecepie(recepie)))
   })
